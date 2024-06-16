ea01538c99fd61ea9d6e1e1ed972a141aae9077f
Apurva <apurwa.rokade@gmail.com>
2024-06-17 00:18:12 +0530

    added fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
+
 import './App.css'
 import Home from './components/Home'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FoodItemModal from './components/FoodItemModal';
-import TopResPune from './components/TopResPune';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
